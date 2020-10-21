3d71ac778a7e5e5e232c33e2318bf5841f5d418d
icersong <icersong@gmail.com>
2020-10-21 11:43:37 +0800

    add auto load mount srv folder

---
+++ php/configs/auto-srv-roots.php
@@ -0,0 +1,54 @@
+<?php
+error_reporting(0); // Set E_ALL for debuging
+
+
+function get_root($path, $alias) {
+   system('echo "'.$path.'" >> '.'/tmp/xxxx.log');
+   $folder = explode('/', $path)[count(explode('/', $path)) - 1];
+   system('echo "'. $folder .'" >> '.'/tmp/xxxx.log');
+  //array_push($opts['roots'], array(
+  return array(
+    'alias'         => $alias,
+    'driver'        => 'LocalFileSystem',
+    'path'          => $path, 
+    'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/'. folder,
+    //'trashHash'     => 't0_Lw',
+    'winHashFix'    => DIRECTORY_SEPARATOR !== '/',
+    'uploadDeny'    => array('all'),
+    'uploadAllow'   => array('image/x-ms-bmp', 'image/gif', 'image/jpeg', 'image/png', 'image/x-icon', 'text/plain'),
+    'uploadOrder'   => array('deny', 'allow'),
+    'accessControl' => 'access'
+  );
+}
+
+
+function get_exec($cmd) {
+    $buffer = "";
+    $handle = popen($cmd, 'r');
+    while(!feof($handle)) {
+         $buffer.=fgets($handle);
+    }
+    pclose($handle);
+    return $buffer;
+}
+
+
+function get_label($dev) {
+    $buffer = get_exec('blkid -p '.$dev.' -s LABEL -o value');
+    if (trim($buffer) !== '') {
+        return trim($buffer);
+    }
+    return trim(get_exec("blkid -p ".$dev." -s PART_ENTRY_UUID -o value"));
+}
+
+
+$buffer = get_exec("mount |grep /srv/dev-disk-by- |awk -F'[ ]' '{print $1 \" \" $3}'");
+foreach(explode("\n",$buffer) as $line) {
+    if (trim($line) === '') { continue; }
+    system('echo "'.$line.'" >> '.'/tmp/xxxx.log');
+    list($dev, $path) = explode(" ", $line);
+    // $label = get_label($dev);
+    list($_empty_, $_root_, $label) = explode('/', $dev);
+    //add_root($opts, $path, $label);
+    array_push($opts['roots'], get_root($path, $label));
+}
