0d619a1aa00757191b4c8f96f887978cbdcc6442
icersong <icersong@gmail.com>
2020-12-03 15:19:41 +0800

    增加 配置函数，将配置移动到函数中实现，并增加一些说明

--- php/configs/auto-srv-roots.php
+++ php/configs/auto-srv-roots.php
@@ -2,6 +2,11 @@
 error_reporting(0); // Set E_ALL for debuging
 
 
+/**
+ * 根据给定信息生成挂载磁盘路径的配置。
+ * 每个新挂载的磁盘，都需要独立配置来完成。
+ * 此函数即用来生成挂载路径的配置信息
+ **/
 function get_root($path, $alias) {
    system('echo "'.$path.'" >> '.'/tmp/xxxx.log');
    $folder = explode('/', $path)[count(explode('/', $path)) - 1];
@@ -22,7 +27,10 @@ function get_root($path, $alias) {
   );
 }
 
-
+/**
+ * 获取命令行执行结果
+ * 执行给定命令，并获取其执行过程标准输出的信息
+ **/
 function get_exec($cmd) {
     $buffer = "";
     $handle = popen($cmd, 'r');
@@ -33,7 +41,9 @@ function get_exec($cmd) {
     return $buffer;
 }
 
-
+/**
+ * 获取挂载路径的"标签"名称
+ **/
 function get_label($dev) {
     $buffer = get_exec('blkid -p '.$dev.' -s LABEL -o value');
     if (trim($buffer) !== '') {
@@ -43,13 +53,20 @@ function get_label($dev) {
 }
 
 
-$buffer = get_exec("mount |grep ' on /srv/'|awk -F'[ ]' '{print $1 \" \" $3}'");
-foreach(explode("\n",$buffer) as $line) {
-    if (trim($line) === '') { continue; }
-    system('echo "'.$line.'" >> '.'/tmp/xxxx.log');
-    list($dev, $path) = explode(" ", $line);
-    // $label = get_label($dev);
-    list($_empty_, $_root_, $label) = explode('/', $dev);
-    //add_root($opts, $path, $label);
-    array_push($opts['roots'], get_root($path, $label));
+/**
+ * 扫描所有已经挂载的路径，并根据挂载的路径生成响应的配置文件，挂载到elFinder根路径下
+ **/
+function get_all($opts) {
+    $buffer = get_exec("mount |grep ' on /srv/'|awk -F'[ ]' '{print $1 \" \" $3}'");
+    foreach(explode("\n",$buffer) as $line) {
+        if (trim($line) === '') { continue; }
+        system('echo "'.$line.'" >> '.'/tmp/xxxx.log');
+        list($dev, $path) = explode(" ", $line);
+        // $label = get_label($dev);
+        list($_empty_, $_root_, $label) = explode('/', $dev);
+        //add_root($opts, $path, $label);
+        array_push($opts['roots'], get_root($path, $label));
+    }
 }
+
+get_all($opts);
